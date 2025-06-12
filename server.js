a932848eee1660f1672466929e8186382d4e8453
Richard <richiemighty5@gmail.com>
2025-06-12 07:12:47 +0100

    mogodb error fixing

--- server.js
+++ server.js
@@ -6,13 +6,6 @@ const http = require('http');
 const socketio = require('socket.io');
 const User = require('./models/User'); // Make sure this path is correct
 
-import dotenv from 'dotenv';
-dotenv.config();
-
-
-const MONGO_URI = process.env.MONGO_URI;
-
-
 // Route files
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
@@ -38,7 +31,7 @@ app.use(cors({
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('✅ Connected to MongoDB'))
 .catch(err => console.error('❌ MongoDB connection error:', err));
 
